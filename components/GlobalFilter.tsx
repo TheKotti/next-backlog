e20176214b9d850ca5c9d654cedffd6ed75be132
Konsta Mustonen <konsta.mustonen@gmail.com>
2022-04-23 18:29:14 +0300

    Use react-table

---
+++ components/GlobalFilter.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { useAsyncDebounce } from 'react-table'
+
+export const GlobalFilter = ({ globalFilter, setGlobalFilter }) => {
+  const [value, setValue] = useState(globalFilter)
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value)
+          onChange(e.target.value)
+        }}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import { useEffect } from 'react'
+/* eslint-disable react/jsx-key */
+import { useEffect, useMemo, useState } from 'react'
 import Head from 'next/head'
 import router from 'next/router'
 import { getSession, signIn } from 'next-auth/react'
+import { useGlobalFilter, useSortBy, useTable } from 'react-table'
+import dayjs from 'dayjs'
 
 import Nav from '../components/Nav'
 import styles from '../styles/Home.module.css'
 import { connectToDatabase } from '../lib/mongo'
+import { GlobalFilter } from '../components/GlobalFilter'
 
 type Props = {
   isAdmin: boolean
@@ -25,6 +29,57 @@ export default function Home({ isAdmin, games = [] }: Props) {
     }
   }
 
+  const columns = useMemo(() => {
+    return [
+      {
+        Header: 'Id',
+        accessor: '_id',
+        disableGlobalFilter: true,
+      },
+      {
+        Header: 'Date',
+        accessor: 'finishedDate',
+        disableGlobalFilter: true,
+      },
+      {
+        Header: 'Game',
+        accessor: 'title',
+      },
+      {
+        Header: 'Rating',
+        accessor: 'rating',
+        disableGlobalFilter: true,
+      },
+    ]
+  }, [])
+
+  const hiddenColumns = useMemo(() => ['_id'], [])
+
+  const data: Array<any> = useMemo(() => {
+    return games.map((x) => {
+      return {
+        _id: x._id,
+        title: x.title,
+        finishedDate: x.finishedDate ? dayjs(new Date(x.finishedDate)).format('DD MMM YYYY') : '', // What the heck next/dayjs?
+        rating: x.rating,
+      }
+    })
+  }, [games])
+
+  const { getTableProps, getTableBodyProps, headers, rows, prepareRow, state, setGlobalFilter } = useTable(
+    {
+      columns,
+      data,
+      initialState: {
+        hiddenColumns,
+      },
+    },
+    useGlobalFilter,
+    useSortBy
+  )
+
+  const { globalFilter } = state
+
   return (
     <div>
       <Head>
@@ -38,26 +93,33 @@ export default function Home({ isAdmin, games = [] }: Props) {
           {games.length === 0 ? (
             <h2></h2>
           ) : (
-            <table>
-              <thead>
-                <tr>
-                  <th>Game</th>
-                </tr>
-              </thead>
-              <tbody>
-                {games.map((game, i) => (
-                  <tr key={game._id}>
-                    <td
-                      onClick={() => {
-                        gameClick(game._id)
-                      }}
-                    >
-                      {game.title}
-                    </td>
+            <>
+              <GlobalFilter globalFilter={globalFilter} setGlobalFilter={(e) => setGlobalFilter(e)} />
+              <table {...getTableProps}>
+                <thead>
+                  <tr>
+                    {headers.map((column) => {
+                      return (
+                        <th {...column.getHeaderProps(column.getSortByToggleProps())}>{column.render('Header')}</th>
+                      )
+                    })}
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+
+                <tbody {...getTableBodyProps()}>
+                  {rows.map((row) => {
+                    prepareRow(row)
+                    return (
+                      <tr {...row.getRowProps()} onClick={() => gameClick(row.values._id)}>
+                        {row.cells.map((cell) => {
+                          return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                        })}
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </>
           )}
         </div>
       </main>
