d3d3f0af6186688b713ae66249377fdec9b1429b
Konsta Mustonen <konsta.mustonen@gmail.com>
2022-09-14 21:08:26 +0300

    Add stats for nerds

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useMemo, useState } from 'react'
 import Head from 'next/head'
 import { getSession, signIn } from 'next-auth/react'
+import Modal from 'react-bootstrap/Modal'
 
 import Nav from '../components/Nav'
 import styles from '../styles/Home.module.css'
@@ -16,6 +17,7 @@ type Props = {
 
 export default function Home({ isAdmin, games = [] }: Props) {
   const [viewBacklog, setViewBacklog] = useState(false)
+  const [show, setShow] = useState(false)
 
   const playedGames = useMemo(
     () =>
@@ -38,6 +40,87 @@ export default function Home({ isAdmin, games = [] }: Props) {
     }) // Should clean this up but fuck next is doing a thing so fuck it for now
   }, [])
 
+  // It's a disaster but whatever, clean up when drunk or something
+  const stats = useMemo(() => {
+    // This filter crap shouldn't be necessary but fuck it
+    const ratings = games.map((x) => x.rating).filter((x): x is number => x !== null)
+    const times = games.map((x) => x.timeSpent).filter((x): x is number => x !== null)
+
+    const averageRating = ratings.reduce((a, b) => a + b, 0) / ratings.length
+    const averageTime = times.reduce((a, b) => a + b, 0) / times.length
+    const sneakyGames = games.filter((x) => x.stealth).length
+    const streamedGames = games.filter((x) => x.streamed).length
+    const finishedGames = games.filter((x) => x.finished && x.finished !== 'Nope').length
+    const droppedGames = games.filter((x) => x.finished && x.finished === 'Nope').length
+    const backlogLength = games.filter((x) => !x.finishedDate).length
+
+    const gamesByYear = games
+      .filter((x) => x.rating)
+      .reduce((acc, obj) => {
+        const key = obj.releaseYear || 0
+        const curGroup = acc[key] ?? []
+
+        if (key === 0) return { ...acc }
+        return { ...acc, [key]: [...curGroup, obj] }
+      }, {})
+    const yearlyAvg = Object.entries(gamesByYear).map(([key, value]) => {
+      const yearlyRatings = (value as Game[]).map((x) => x.rating)
+      const avg =
+        yearlyRatings.length >= 5 ? (yearlyRatings as number[]).reduce((a, b) => a + b, 0) / yearlyRatings.length : 0
+      return { key, avg }
+    })
+    const bestYear = yearlyAvg.reduce((prev, current) => {
+      return prev.avg > current.avg ? prev : current
+    }).key
+    const bestYearGames = games
+      .filter((x) => x.rating && x.rating >= 7 && x.releaseYear?.toString() === bestYear)
+      .map((x) => x.title)
+      .join(', ')
+
+    const longest = games
+      .filter((x) => x.timeSpent && x.finished !== 'Nope')
+      .reduce((longest, game) => (longest.timeSpent! > game.timeSpent! ? longest : game))
+
+    return [
+      {
+        key: 'Average rating',
+        value: averageRating.toFixed(2),
+      },
+      {
+        key: 'Average time spent',
+        value: `${averageTime.toFixed(2)}h`,
+      },
+      {
+        key: 'Streamed games',
+        value: streamedGames,
+      },
+      {
+        key: 'Finished games',
+        value: finishedGames,
+      },
+      {
+        key: 'Dropped games',
+        value: droppedGames,
+      },
+      {
+        key: 'Sneaky games',
+        value: sneakyGames,
+      },
+      {
+        key: 'Games in backlog',
+        value: backlogLength,
+      },
+      {
+        key: 'Most time spent',
+        value: `${longest.title} (${longest.timeSpent}h)`,
+      },
+      {
+        key: 'Best year for games, objectively',
+        value: `${bestYear} (${bestYearGames})`,
+      },
+    ]
+  }, [games])
+
   return (
     <div>
       <Head>
@@ -50,6 +133,10 @@ export default function Home({ isAdmin, games = [] }: Props) {
         <div className={styles.container}>
           <div className={`d-flex justify-content-between mb-3 ${styles.header}`}>
             <h1>{viewBacklog ? 'Backlog' : 'Previously played'}</h1>
+
+            <button className='btn btn-primary' onClick={() => setShow(true)}>
+              Stats for nerds
+            </button>
 
             <button className='btn btn-primary' onClick={() => setViewBacklog(!viewBacklog)}>
               {viewBacklog ? 'Show previously played' : 'Show backlog'}
@@ -69,6 +156,31 @@ export default function Home({ isAdmin, games = [] }: Props) {
           )}
         </div>
       </main>
+
+      <Modal show={show} onHide={() => setShow(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Stats for nerds</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <table className='w-100'>
+            <tbody>
+              {stats.map(({ key, value }) => {
+                return (
+                  <tr className='border-bottom lh-lg'>
+                    <td className='w-50'>{key}</td>
+                    <td>{value}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className='btn btn-light' onClick={() => setShow(false)}>
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
