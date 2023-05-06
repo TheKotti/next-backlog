f7894b2823fe5ba93c673d82cc4adb006bf9431f
Konsta Mustonen <konsta.mustonen@gmail.com>
2023-05-06 17:59:41 +0300

    Filter duplicate devs

--- components/RandomGame.tsx
+++ components/RandomGame.tsx
@@ -18,7 +18,7 @@ export default function RandomGame(props: Props) {
       <h1 className={styles.gameTitle} onClick={() => window.open(game.igdbUrl, '_blank')}>
         {game.title} ({game.releaseYear})
       </h1>
-      <p className={styles.gameDeveloper}>{game.developers.join(', ')}</p>
+      <p className={styles.gameDeveloper}>{[...new Set(game.developers)].join(', ')}</p>
       <p className={styles.gameGenres}>{game.keywords.join(', ')}</p>
 
       <button onClick={() => vetoGame(game._id!)} className={`btn btn-light ${styles.gameButton}`}>
