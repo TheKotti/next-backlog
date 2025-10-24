0c8cc1e3de05460876f3f687a5649b8dec94970d
Kotti <kottistream@gmail.com>
2025-10-24 11:34:46 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts',
+    ]),
+])
 
-export default defineConfig([{
-    extends: [...nextCoreWebVitals],
-
-    rules: {
-        "@next/next/no-img-element": "off",
-        "react/no-unescaped-entities": "off",
-    },
-}]);
+export default eslintConfig
