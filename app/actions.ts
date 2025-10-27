358c1fcd82b3d12fad9df2d021582657f2fc6b60
Kotti <kottistream@gmail.com>
2025-10-27 11:27:28 +0100

    revalidate as action

--- app/actions.ts
+++ app/actions.ts
@@ -1,8 +1,24 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
 import { ObjectId } from "mongodb"
 import { auth } from "./auth"
 const { connectToDatabase } = require('lib/mongo')
+
+export async function revalidateAction() {
+    try {
+        const authState = await auth()
+        const username = authState?.user?.name ?? ""
+        const isAdmin = process.env.ADMIN_USER_NAME === username
+        if (isAdmin) {
+            await revalidatePath('/goty')
+            return true
+        }
+    } catch (error) {
+        console.log('Error revalidating:', error)
+        return false
+    }
+}
 
 export async function updateVodsAction(formData: FormData) {
     try {
