a2562666821a1183a0d53068839a411d70f72936
Kotti <kottistream@gmail.com>
2025-10-28 16:31:04 +0100

    nextjs auth fix maybe

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
-import { handlers } from "app/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
-export { auth as middleware } from "app/auth"
+import { handlers } from "app/auth"
+import { auth as middleware } from "app/auth"
+const { GET, POST } = handlers
+
+export { middleware, GET, POST }
