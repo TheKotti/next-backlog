d3768e177efce34984b670287accb9bff79a193f
Kotti <kottistream@gmail.com>
2025-11-05 15:59:13 +0100

    maybe auth tests

--- app/auth.ts
+++ app/auth.ts
@@ -1,13 +1,28 @@
-import NextAuth from "next-auth"
+import NextAuth, { User } from "next-auth"
 import TwitchProvider from "next-auth/providers/twitch"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const realProviders: any = [
+    TwitchProvider({
+        clientId: process.env.TWITCH_CLIENT_ID,
+        clientSecret: process.env.TWITCH_CLIENT_SECRET
+    })
+]
+
+const testProviders: any = [
+    CredentialsProvider({
+        name: 'Credentials',
+        async authorize(_credentials, _req): Promise<User> {
+            return {
+                id: "111",
+                name: "testuser"
+            }
+        }
+    })
+]
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        TwitchProvider({
-            clientId: process.env.TWITCH_CLIENT_ID,
-            clientSecret: process.env.TWITCH_CLIENT_SECRET
-        })
-    ],
+    providers: process.env.ADMIN_USER_NAME == "testuser" ? testProviders : realProviders,
     callbacks: {
         async jwt({ token, account }) {
             if (account) {
---
+++ tests/adminlist.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect, Page } from '@playwright/test'
+
+const login = async (page: Page) => {
+  await page.goto('/admin')
+  await page.click('text=Sign in')
+  await page.click('text=Sign in with Credentials')
+  await page.goto('/admin')
+}
+
+test('should log in to admin view', async ({ page }) => {
+  await login(page)
+
+  await page.isVisible('text=Sign Out (testuser)')
+  await page.isVisible('text=Game list')
+  await page.isVisible('text=Add game')
+  await page.isVisible('text=Revalidate')
+  await page.isVisible('text=Previously played')
+})
+
+test('should show admin played games list', async ({ page }) => {
+  await login(page)
+
+  const tbody = await page.locator('css=tbody')
+  const rows = await tbody.getByRole("row")
+  await expect(rows).toHaveCount(10)
+
+  const expected = ["date", "game", "rating", "comments", "finished", "vods", "admin"]
+  const headers = await page.locator("css=th").all()
+
+  for (let index = 0; index < headers.length; index++) {
+    await expect(headers[index]).toContainText(expected[index], { ignoreCase: true })
+  }
+})
+
+test('should navigate to recap', async ({ page }) => {
+  await login(page)
+
+  await page.getByPlaceholder('Search').fill("asghan")
+  const tbody = await page.locator('css=tbody')
+  const rows = await tbody.getByRole("row")
+
+  await expect(rows).toHaveCount(1)
+  await expect(tbody).toContainText("Asghan")
+  await page.click('text=Recap')
+  await expect(page.locator('h1')).toContainText('Asghan: The Dragon Slayer')
+})
