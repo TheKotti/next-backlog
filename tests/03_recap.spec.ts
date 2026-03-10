602da4802674b4aeb77bcc4046ce26576e28a3c0
Kotti <konsta.mustonen@gmail.com>
2026-03-10 14:24:26 +0200

    jank fix

--- tests/03_recap.spec.ts
+++ tests/03_recap.spec.ts
@@ -37,15 +37,13 @@ test('should change comment area font size', async ({ page }) => {
 
     const commentArea = await page.locator('css=#commentArea')
 
-    await expect(commentArea).toContainText(
-        'The story had a bit of a slow start'
-    )
-
-    await expect(commentArea).toHaveCSS('font-size', '16px')
     await commentArea.fill('short text')
-    await expect(commentArea).toHaveCSS('font-size', '32px')
+
+    await expect(commentArea).toHaveCSS('font-size', '32x')
     await commentArea.fill(longtext)
     await expect(commentArea).toHaveCSS('font-size', '16px')
+    await commentArea.fill('short text')
+    await expect(commentArea).toHaveCSS('font-size', '32x')
 })
 
 test('should save changes to played game', async ({ page }) => {
